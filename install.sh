72f2d08d1a477100228784b1d28d0e71b06de2ba
STS Education <isabella.gibson@stseducation-us.com>
2022-06-08 07:05:36 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+clear
+sudo apt update
+sudo apt upgrade -y
+sudo apt install neofetch net-tools -y
+sudo echo "healthy:x:1000:1000:Healthy Player One,,,:/home/healthy:/bin/bash" >> /etc/passwd
+sudo usermod -aG sudo username
